7a98fdc200cfb7aa86a3718e2e539e2aebd7fc73
EinsteinTesla <moumitachin@gmail.com>
2021-03-18 20:59:33 +0530

    Giving title to game in pygame.

---
+++ Giving Title in Gaemwindow and Variables.py
@@ -0,0 +1,13 @@
+import pygame
+
+x = pygame.init()
+
+# Creating Window :- 
+
+gameWindow = pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("My First Game")
+
+# Game specific variables in pygame :-
+
+exit_game = False # If exit_game = True the it will close the window
+game_over = False # If game_over = True then the game will be over.
