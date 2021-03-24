ef6b9c406f9ca4b5d13432df2b64b58f10f307c8
EinsteinTesla <moumitachin@gmail.com>
2021-03-24 19:51:16 +0530

    Adding comments.

--- Dragon game/main.py
+++ Dragon game/main.py
@@ -3,15 +3,32 @@ import sys
 import time
 import os
 
+# Initializing pygame modules :-
+
 pygame.init()
+
+# Setting screen width and height :-
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
+
+# Setting font style :-
+
 font = pygame.font.SysFont("Arial", 60)
+
+# Initialising clock :-
+
 clock = pygame.time.Clock()
 
+# Initialising game window :-
+
 game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Setting caption :-
+
 pygame.display.set_caption("Dragon Game by Aritra")
+
+# Configuring images :-
 
 welcome_image = pygame.image.load('gallery/sprites/welcome_dragon.jpg')
 welcome_image = pygame.transform.scale(welcome_image, (SCREEN_WIDTH, SCREEN_HEIGHT)).convert_alpha()
@@ -21,6 +38,9 @@ game_over = pygame.image.load('gallery/sprites/game_over.png')
 game_over = pygame.transform.scale(game_over, (SCREEN_WIDTH, SCREEN_HEIGHT)).convert_alpha()
 life = pygame.image.load('gallery/sprites/life.png')
 life = pygame.transform.scale(life, (20, 20))
+
+# Game Sprites :-
+
 GAME_SPRITES = {'dino': pygame.image.load('gallery/sprites/dino.png').convert_alpha(),
                 'dragon': pygame.image.load('gallery/sprites/dragon.png').convert_alpha(),
                 'background': back_image,
@@ -28,6 +48,8 @@ GAME_SPRITES = {'dino': pygame.image.load('gallery/sprites/dino.png').convert_al
                 'game-over': game_over,
                 'life': life
                 }
+
+# Game sounds :-
 
 GAME_SOUNDS = {
     'die': pygame.mixer.Sound('gallery/Audio/die.wav'),
@@ -37,10 +59,14 @@ GAME_SOUNDS = {
 }
 
 
+# Shows text on the screen :-
+
 def display_text(text, color, x, y):
     screen_text = font.render(text, True, color)
     game_window.blit(screen_text, [x, y])
 
+
+# Displays welcome screen :-
 
 def welcome_screen():
     while True:
@@ -55,6 +81,8 @@ def welcome_screen():
         clock.tick(60)
 
 
+# Checks if there is a collision :-
+
 def is_collide(player_x, player_y, attacker_list):
     collision = 0
     for x, y in attacker_list:
@@ -67,10 +95,14 @@ def is_collide(player_x, player_y, attacker_list):
         return False
 
 
+# Displays dragons on the screen :-
+
 def dragons(dragon_list):
     for x, y in dragon_list:
         game_window.blit(GAME_SPRITES['dragon'], (x, y))
 
+
+# Displays the game_over screen :-
 
 def game_over():
     while True:
@@ -85,12 +117,18 @@ def game_over():
         pygame.display.update()
 
 
+# Displays the number of lives available :-
+
 def show_life(coordinates):
     for x, y in coordinates:
         game_window.blit(GAME_SPRITES['life'], (x, y))
 
 
+# Main Game :-
+
 def main_game():
+    # Game variables :-
+
     t = time.time()
     x = time.time()
     cross = True
@@ -111,10 +149,18 @@ def main_game():
     life_deducted = True
     life_deduction_delay = delay
 
+    # Creating a high_score file if not exists :-
+
     if not os.path.exists('high_score.txt'):
         with open('high_score.txt', 'w') as f:
             f.write('0')
+
+    # Running the game_loop :-
+
     while True:
+
+        # Checking for the events :-
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -130,7 +176,12 @@ def main_game():
             if event.type == pygame.KEYUP:
                 vel_x = 0
                 vel_y = 0
+
+        # Stores the co-ordinates of the attacker :-
+
         attacker = [[attacker_x, attacker_y]]
+
+        # Check for the current score and increases difficulty according to it :-
         if score > 10:
             attacker.append([attacker_x + 500, attacker_y])
         if score > 20:
@@ -145,13 +196,29 @@ def main_game():
             limit = -1500
         if score > 70:
             vel_attacker_x = -10
+
+        # Increases the co-ordinates of the player:-
         player_x += vel_x
         player_y += vel_y
+
+        # Shows the background :-
+
         game_window.blit(GAME_SPRITES['background'], (0, 0))
+
+        # Displays the player on the screen :-
+
         game_window.blit(GAME_SPRITES['dino'], (player_x, player_y))
+
+        # Increases the x co-ordinate of the attacker :-
+
         attacker_x += vel_attacker_x
+
+        # Places the dragon(attacker) in the starting position after it goes out of the screen :-
         if attacker_x <= limit:
             attacker_x = SCREEN_WIDTH + 100
+
+        # Jump the player :-
+
         if player_jumped:
             if jump_count >= -10:
                 player_y -= (jump_count * abs(jump_count)) * 0.5
@@ -160,6 +227,9 @@ def main_game():
             else:
                 jump_count = 10
                 player_jumped = False
+
+        # Increases the score :-
+
         if cross:
             for j, k in attacker:
                 if abs(player_x - j) < 145:
@@ -168,26 +238,51 @@ def main_game():
                     timer = True
                     cross = False
                     GAME_SOUNDS['point'].play()
+
+        # Updates the boolean value of cross and life_deducted after a certain time :-
         if timer:
             if time.time() - x >= delay:
                 cross = True
             if time.time() - t >= life_deduction_delay:
                 life_deducted = True
+
+        # Checks if collision happened and reduces 1 life :-
+
         if is_collide(player_x, player_y, attacker) and life_deducted:
             t = time.time()
             del life_coordinates[len(life_coordinates) - 1]
             life_deducted = False
+
+        # Checks the number of lives available and if it is 0 calls the game_over function.
+
         if len(life_coordinates) == 0:
             GAME_SOUNDS['hit'].play()
+
+            # Writes the new high_score after the game gets over :-
+
             with open('high_score.txt', 'w') as z:
                 z.write(str(score))
             game_over()
             GAME_SOUNDS['die'].play()
+
+        # Passes the co_ordinates of the dragon(attacker) to the dragon function for displaying :-
         dragons(attacker)
+
+        # Passing the score to display_text for displaying on the screen :-
+
         display_text(str(score), (255, 0, 0), 793, 48)
+
+        # Passes the life_coordinates to show_life function for displaying on the screen:-
         show_life(life_coordinates)
+
+        # Updating the display :-
+
         pygame.display.update()
+
+        # Ticking the clock 60 times a second.
         clock.tick(60)
 
+
+# Calling the welcome screen :-
 
 welcome_screen()
